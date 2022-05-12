b2e734f5198457d360af8d5b482fa4d80ed93179
IJBILI <imen.jbili@gmail.com>
2022-05-12 11:27:12 +0200

    first commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app-imen:app
+web: gunicorn app-api:app
--- app-api.py
+++ app-api.py
@@ -13,7 +13,7 @@ import numpy as np
 app = Flask(__name__)
 randomForest = pickle.load(open('./classifier_rf_model.sav', 'rb'))
 sample_size = 10000
-data_set = pd.read_csv('./dataset_exported.csv',nrows=sample_size)
+data_set = pd.read_csv('./data_test.csv',nrows=sample_size)
 
 @app.route('/predictByClientId', methods=['POST'])
  
@@ -24,13 +24,13 @@ def predictByClientId():
             json_ = request.json
             print(json_)
             
-            client=data_set[data_set['SK_ID_CURR']==json_['SK_ID_CURR']].drop(['SK_ID_CURR','TARGET'],axis=1)
+            client=data_set[data_set['SK_ID_CURR']==int(json_['SK_ID_CURR'])].drop(['SK_ID_CURR','TARGET'],axis=1)
             print(client)
             
             y_pred = randomForest.predict(client)
             y_proba = randomForest.predict_proba(client)
             
-            return jsonify({'prediction': str(y_pred[0]),'prediction_proba':str(y_proba[0][0])})
+            return jsonify({'prediction': str(y_pred[0]),'prediction_proba':y_proba[0][0]})
 
 
         except:
